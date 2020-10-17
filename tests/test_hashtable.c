80754bc65057336b9907e4be87afaa9e03c84640
Eduardo Fischer <efischer@outlook.com.br>
2020-10-16 23:17:24 -0300

    #23 - Melhora a função de hash (djb2)

--- tests/test_hashtable.c
+++ tests/test_hashtable.c
@@ -6,6 +6,7 @@ int main() {
   HT_ENTRY **table1 = malloc(sizeof(HT_ENTRY) * HT_SIZE);
   HT_ENTRY *entry1;
 
+  char key[20] = "testechave";
   SYMBOL_ENTRY s1;
   s1.line = 10;
   s1.column = 20;
@@ -14,10 +15,10 @@ int main() {
   s1.arguments = NULL;
   s1.value.i = 30;
 
-  int index = insert_ht_entry(table1, "chave_teste", s1);
+  int index = insert_ht_entry(table1, key, s1);
   printf("Entrada inserida na tabela na posição %d\n", index);
 
-  entry1 = get_ht_entry(table1, "chave_teste");
+  entry1 = get_ht_entry(table1, key);
   printf("Entrada recuperada na tabela. Chave: %s\n", entry1->key);
 
   free_ht(table1);
